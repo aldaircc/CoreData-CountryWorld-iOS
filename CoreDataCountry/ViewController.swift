79fe7e878671737dcc2a07fd3c64227aa7840294
Aldair Cosetito Coral <aldair.soft@hotmail.com>
2020-09-28 22:09:49 -0500

    Initial Commit

---
+++ CoreDataCountry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataCountry
+//
+//  Created by Aldair Raul Cosetito Coral on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
